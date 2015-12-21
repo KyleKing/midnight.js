a909bc203901c1cbc0b138547c10c61acba28bc4
Kyle King <kmking72@gmail.com>
2015-12-21 12:03:11 -0500

    Ready to post to Crater.io

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'kyleking:midnightjs',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'A jQuery plugin to switch fixed headers on the fly',
   // URL to the Git repository containing the source code for this package.
